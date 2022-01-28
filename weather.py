9e429d4547c976a05862e953d3b82b3cc7d130d3
Rapasi <Rapasi@users.noreply.github.com>
2022-01-28 13:51:33 +0200

    Merenkorkeus lisätty

--- weather.py
+++ weather.py
@@ -29,7 +29,7 @@ def weather_downnload(paikka):
 
   asema=(sorted(obs.data[latest_tstep].keys()))
 
-  print(obs.data[latest_tstep][asema[0]].keys())
+  #print(obs.data[latest_tstep][asema[0]].keys())
 
   # Selecting observations 
   keys=['Air temperature','Wind speed','Wind direction','Snow depth','Pressure (msl)','Cloud amount']
@@ -55,39 +55,66 @@ def weather_downnload(paikka):
   pressure_value=pressure['value']
   snow_value=snow['value']
   wind_dir=wind_direction['value']
-  cloud_amount=arvot[-1]['units']
-  print(cloud_amount)
-
-  # This function will convert wind direction to compass direction 
-
-  def degrees_to_cardinal(d):
-      '''
-      note: this is highly approximate...
-      '''
-      dirs = ["Pohjoisesta",  "Koillisesta",  "Idästä",  "Kaakosta", 
-              "Etelästä",  "Lounaasta",  "Lännestä",  "Luoteesta"]
-      ix = round(d / (360. / len(dirs)))
-      return dirs[ix % len(dirs)]
-
 
   # Checking if snow value is nan
 
   if math.isnan(snow_value):
     snow_value='ei saatavilla'
 
-  # a Function to round the wind direction to nearest 5.
 
-  def myround(x, base=5):
-      return base * round(x/base)
+  # Listing weather observation from the nearest station. 
+  value_list=[asema[0],temp_value,wind_value,wind_dir,pressure_value,snow_value]
+  return(value_list)
 
-  # Printing out current wether in nearest station to location. 
 
-  return(f"{asema[0]}\n\n"
-        f"Lämpötila {temp_value}\u00b0C\n"
-        f'Tuuli {wind_value} m/s {degrees_to_cardinal(wind_dir) } ({myround(wind_dir)}\u00b0)\n'
-        f'Lumen syvyys {snow_value}\n'
-        f'Ilmanpaine {pressure_value} hPa'
-        )
+# This function will convert wind direction to compass direction 
+def degrees_to_cardinal(d):
+    '''
+    note: this is highly approximate...
+    '''
+    dirs = ["Pohjoisesta",  "Koillisesta",  "Idästä",  "Kaakosta", 
+            "Etelästä",  "Lounaasta",  "Lännestä",  "Luoteesta"]
+    ix = round(d / (360. / len(dirs)))
+    return dirs[ix % len(dirs)]
+
+# a Function to round the wind direction to nearest 5.
+
+def myround(x, base=5):
+    return base * round(x/base)
+
+def sea_download(location):
+  from cmath import nan
+  import datetime as dt
+  from xml.etree.ElementPath import prepare_self
+  import math
+  from fmiopendata.wfs import download_stored_query
+
+  end_time = dt.datetime.utcnow()
+  start_time = end_time - dt.timedelta(minutes=60)  
+
+  # Retrieve the latest hour of data from a bounding box
+  end_time = dt.datetime.utcnow()
+  start_time = end_time - dt.timedelta(minutes=60)
+  print(start_time)
+  # Convert times to properly formatted strings
+  start_time = start_time.isoformat(timespec="seconds") + "Z"
+  # -> 2020-07-07T12:00:00Z
+  end_time = end_time.isoformat(timespec="seconds") + "Z"
+
+  obs = download_stored_query("fmi::forecast::oaas::sealevel::point::multipointcoverage",
+                              args=["starttime=" + start_time,
+                                    "endtime=" + end_time])
+
+  latest_tstep = max(obs.data.keys())
+  asema=(sorted(obs.data[latest_tstep].keys()))
   
+
+  values=obs.data[latest_tstep][str(location).capitalize()]
+  for k, v in values.items():
+    values=(k, v)
+  sea_data=[list(values[1].values())[0],asema]
+  return(sea_data)
+  
+
 if __name__=='__main__':
   pass
--- weather_gui.py
+++ weather_gui.py
@@ -1,34 +1,69 @@
+import sqlite3 
+
 # Using Tkinter for Gui
 import tkinter as tk
 
 #Importing weather_downnload function from weather_downnload.py
-from weather import weather_downnload
+from weather import *
 
 #Get users entry and show downloaded wether in label
 def get_entry():
     try:
         weather=weather_downnload(entry.get())
-        label2['text']=weather
-    except Exception: 
-        label2['text']='Select valid location'
+        label2['text']=(f"{weather[0]}\n\n"
+        f"Lämpötila {weather[1]}\u00b0C\n"
+        f'Tuuli {weather[2]} m/s {degrees_to_cardinal(weather[3]) } ({myround(weather[3])}\u00b0)\n'
+        f'Ilmanpaine {weather[4]} hPa\n'
+        f'Lumen syvyys {weather[5]} \n')
+    except Exception as e: 
+        print(e)
+        label2['text']='Syötä kelvollinen sijainti'
+
+def get_sea():
+    try:
+        sea_level=sea_download(entry2.get())
+        mittausasema=sea_download(entry2.get().capitalize())[1]
+        for i in mittausasema:
+            if i==entry2.get().capitalize():
+                observation=i
+
+        label2['text']=(f'Mittausasema {observation}\n'
+        f'Vedenkorkeus on {sea_level[0]} cm')
+    except Exception as e:
+        print(e)
+        label2['text']='Vedenkorkeus ei saatavilla valitulle paikalle'
+
+
 
 # Usual Tkinter parameters
 root=tk.Tk()
 root.geometry('500x500')
 
-label=tk.Label(root,text='Valitse paikka',fg='black',height=2)
+label=tk.Label(root,text='Valitse paikka (sää)',fg='black',height=2)
 label.place(relx=.1)
+
+label2=tk.Label(root,text='Valitse paikka (vedenkorkeus)',fg='black',height=2)
+label2.place(relx=.1,rely=.2)
 
 entry=tk.Entry(root)
 entry.place(anchor='center',relx=.2,rely=.1)
 
+entry2=tk.Entry(root)
+entry2.place(anchor='center',relx=.2,rely=.3)
+
 button=tk.Button(root,text='Lataa säätiedot',command=lambda:get_entry())
 button.place(anchor='center',relx=.5,rely=.1)
 
+button2=tk.Button(root,text='Lataa vedenkorkeus',command=lambda:get_sea())
+button2.place(anchor='center',relx=.5,rely=.3)
+
+
 frame=tk.Frame(root,bd=10)
-frame.place(relx=.4,rely=.2,anchor='n',relwidth=.75,relheight=.6)
+frame.place(relx=.4,rely=.5,anchor='n',relwidth=.75,relheight=.6)
 
 label2=tk.Label(frame,font=("Arial", 16))
 label2.place(relx=.02,rely=.1)
 
+
 root.mainloop()
+    
